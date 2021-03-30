e8dbf133298f68250ed22784d229a18e22ddcaa5
Eward <33430641+pannekoek141@users.noreply.github.com>
2021-03-30 21:16:16 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,24 @@
 // JavaScript Document
+
+//Haalt alle items op met de classname accordionItem en slaat deze op in een array(collectie van items)
 var accItem = document.getElementsByClassName('accordionItem');
-var accHD = document.getElementsByClassName('accordionItemHeading');
-for (i = 0; i < accHD.length; i++) {
-    accHD[i].addEventListener('click', toggleItem, false);
+
+//Voor elke item in de array accItem voeg de event listener CLICK toe
+for (i = 0; i < accItem.length; i++) {
+    accItem[i].addEventListener('click', toggleItem);
+    //wanneer click triggert, voer de functie toggleItem uit
 }
+
+//deze functie triggert dus als de eventlistener click uitgevoerd wordt
 function toggleItem() {
-    var itemClass = this.parentNode.className;
+    //voor elke item in de array accItem
     for (i = 0; i < accItem.length; i++) {
+        //voeg de class close toe
         accItem[i].className = 'accordionItem close';
-    }
-    if (itemClass == 'accordionItem close') {
-        this.parentNode.className = 'accordionItem open';
+        
+        //als de classname al close is, voeg open class toe
+        if (accItem[i].className == 'accordionItem close') {
+        this.className = 'accordionItem open';
+        }
     }
 }
